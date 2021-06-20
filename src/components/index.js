e1edae816660dd5a4f9a5c6488904fbefc6b322a
ender <ender@localhost>
2021-06-20 19:18:51 +0200

    init commit

--- src/components/index.js
+++ src/components/index.js
@@ -34,6 +34,12 @@ import tooltip from "../directives/tooltip.js"
     app.component("v-list", vList);
     app.component("v-list-item", vListItem);
     app.component("v-collapse", vCollapse);
+    app.component("v-modal", vModal);
+    app.component("v-badge", vBadge);
+    app.component("v-popover", vPopover);
+    app.component("v-spinner", vSpinner);
+    app.component("v-sidepanel", vSidepanel);
+    app.component("v-close-button", vCloseButton);
     app.directive("tooltip", tooltip);
   }
 
--- vite.config.js
+++ vite.config.js
@@ -6,21 +6,21 @@ const path = require('path')
 export default defineConfig({
   plugins: [vue()],
    build: {
-    // lib: {
-    //   entry: path.resolve(__dirname, 'src/components/index.js'),
-    //   name: 'MyLib'
-    // },
-    // rollupOptions: {
+    lib: {
+      entry: path.resolve(__dirname, 'src/components/index.js'),
+      name: 'MyLib'
+    },
+    rollupOptions: {
       // make sure to externalize deps that shouldn't be bundled
       // into your library
-      // external: ['vue'],
-      // output: {
+      external: ['vue'],
+      output: {
         // Provide global variables to use in the UMD build
         // for externalized deps
-        // globals: {
-          // vue: 'Vue'
-        // }
-      // }
-    // }
+        globals: {
+          vue: 'Vue'
+        }
+      }
+    }
   }
 })
