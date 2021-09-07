0e2a136e70d3e0c572927e60500a95129ac4dfb6
maciejg-git <ender@localhost>
2021-09-07 15:06:54 +0200

    added triangle

--- src/components/vChevron.vue
+++ src/components/vChevron.vue
@@ -2,34 +2,51 @@
   <svg
     xmlns="http://www.w3.org/2000/svg"
     :class="[classes.chevron.value, ['transform duration-150', getRotation()]]"
-    fill="none"
-    viewBox="0 0 24 24"
-    stroke="currentColor"
+    width="16"
+    height="16"
+    fill="currentColor"
+    viewBox="0 0 16 16"
   >
-    <path v-if="dir == 'd'"
-      stroke-linecap="round"
-      stroke-linejoin="round"
-      stroke-width="2"
-      d="M19 9l-7 7-7-7"
-    />
-    <path v-else-if="dir == 'l'"
-      stroke-linecap="round"
-      stroke-linejoin="round"
-      stroke-width="2"
-      d="M15 19l-7-7 7-7"
-    />
-    <path v-else-if="dir == 'r'"
-      stroke-linecap="round"
-      stroke-linejoin="round"
-      stroke-width="2"
-      d="M9 5l7 7-7 7"
-    />
-    <path v-else-if="dir == 'u'"
-      stroke-linecap="round"
-      stroke-linejoin="round"
-      stroke-width="2"
-      d="M5 15l7-7 7 7"
-    />
+    <template v-if="triangle">
+      <path
+        v-if="dir == 'd'"
+        d="M7.247 11.14 2.451 5.658C1.885 5.013 2.345 4 3.204 4h9.592a1 1 0 0 1 .753 1.659l-4.796 5.48a1 1 0 0 1-1.506 0z"
+      />
+      <path
+        v-if="dir == 'l'"
+        d="m3.86 8.753 5.482 4.796c.646.566 1.658.106 1.658-.753V3.204a1 1 0 0 0-1.659-.753l-5.48 4.796a1 1 0 0 0 0 1.506z"
+      />
+      <path
+        v-if="dir == 'r'"
+        d="m12.14 8.753-5.482 4.796c-.646.566-1.658.106-1.658-.753V3.204a1 1 0 0 1 1.659-.753l5.48 4.796a1 1 0 0 1 0 1.506z"
+      />
+      <path
+        v-if="dir == 'u'"
+        d="m7.247 4.86-4.796 5.481c-.566.647-.106 1.659.753 1.659h9.592a1 1 0 0 0 .753-1.659l-4.796-5.48a1 1 0 0 0-1.506 0z"
+      />
+    </template>
+    <template v-else>
+      <path
+        v-if="dir == 'd'"
+        fill-rule="evenodd"
+        d="M1.646 4.646a.5.5 0 0 1 .708 0L8 10.293l5.646-5.647a.5.5 0 0 1 .708.708l-6 6a.5.5 0 0 1-.708 0l-6-6a.5.5 0 0 1 0-.708z"
+      />
+      <path
+        v-if="dir == 'l'"
+        fill-rule="evenodd"
+        d="M11.354 1.646a.5.5 0 0 1 0 .708L5.707 8l5.647 5.646a.5.5 0 0 1-.708.708l-6-6a.5.5 0 0 1 0-.708l6-6a.5.5 0 0 1 .708 0z"
+      />
+      <path
+        v-if="dir == 'r'"
+        fill-rule="evenodd"
+        d="M4.646 1.646a.5.5 0 0 1 .708 0l6 6a.5.5 0 0 1 0 .708l-6 6a.5.5 0 0 1-.708-.708L10.293 8 4.646 2.354a.5.5 0 0 1 0-.708z"
+      />
+      <path
+        v-if="dir == 'u'"
+        fill-rule="evenodd"
+        d="M7.646 4.646a.5.5 0 0 1 .708 0l6 6a.5.5 0 0 1-.708.708L8 5.707l-5.646 5.647a.5.5 0 0 1-.708-.708l6-6z"
+      />
+    </template>
   </svg>
 </template>
 
@@ -41,6 +58,7 @@ export default {
     name: { type: String, default: "chevron" },
     styleChevron: { type: String, default: "" },
     dir: { type: String, default: "d" },
+    triangle: { type: Boolean, default: false },
     counterClockwise: { type: Boolean, default: false },
     switch: { type: Boolean, default: false },
   },
@@ -51,8 +69,8 @@ export default {
 
     let getRotation = () => {
       if (props.switch) {
-        if (!props.counterClockwise) return "rotate-90"
-        else if (props.counterClockwise) return "-rotate-90"
+        if (!props.counterClockwise) return "rotate-90";
+        else if (props.counterClockwise) return "-rotate-90";
       }
     };
 
