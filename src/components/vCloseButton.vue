1a1a6ee8e3aceb1d380dee6017ae4ae8ed5ab590
maciejg-git <ender@localhost>
2021-09-07 15:07:36 +0200

    change to bootstrap icons

--- src/components/vCloseButton.vue
+++ src/components/vCloseButton.vue
@@ -3,15 +3,13 @@
     <svg
       xmlns="http://www.w3.org/2000/svg"
       :class="classes.closeButton.value"
-      fill="none"
-      viewBox="0 0 24 24"
-      stroke="currentColor"
+      width="16"
+      height="16"
+      fill="currentColor"
+      viewBox="0 0 16 16"
     >
       <path
-        stroke-linecap="round"
-        stroke-linejoin="round"
-        stroke-width="2"
-        d="M6 18L18 6M6 6l12 12"
+        d="M1.293 1.293a1 1 0 0 1 1.414 0L8 6.586l5.293-5.293a1 1 0 1 1 1.414 1.414L9.414 8l5.293 5.293a1 1 0 0 1-1.414 1.414L8 9.414l-5.293 5.293a1 1 0 0 1-1.414-1.414L6.586 8 1.293 2.707a1 1 0 0 1 0-1.414z"
       />
     </svg>
   </button>
@@ -28,9 +26,9 @@ export default {
   setup(props) {
     let { classes } = useStyles("close-button", props, {
       closeButton: {
-        name: "close-button"
+        name: "close-button",
       },
-    })
+    });
 
     return {
       classes,
