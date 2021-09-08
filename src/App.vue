d4598f2330ae0122f0daa9dc1314ce33e5345a55
maciejg-git <ender@localhost>
2021-09-09 00:08:52 +0200

    remove styles

--- src/App.vue
+++ src/App.vue
@@ -3,39 +3,4 @@
 </template>
 
 <style>
-.tooltip-anim-in {
-  animation: fadeIn 0.3s;
-}
-
-.tooltip-anim-out {
-  animation: fadeOut 0.3s;
-}
-
-@keyframes fadeIn {
-  from {
-    opacity: 0;
-    /* transform: scale(0); */
-    /* top: 20px; */
-  }
-
-  to {
-    opacity: 1;
-    /* transform: scale(1); */
-    /* top: 0px; */
-  }
-}
-
-@keyframes fadeOut {
-  from {
-    opacity: 1;
-    /* transform: scale(1); */
-    /* top: 0px; */
-  }
-
-  to {
-    opacity: 0;
-    /* transform: scale(0); */
-    /* top: 20px; */
-  }
-}
 </style>
