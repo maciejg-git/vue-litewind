dab3681d18a745f99b96769629e2c2195379ead5
maciejg-git <ender@localhost>
2021-09-11 00:14:52 +0200

    add accordion

---
+++ src/components/vAccordion.vue
@@ -0,0 +1,22 @@
+<template>
+  <slot name="default"></slot>
+</template>
+
+<script>
+import { ref, provide } from "vue";
+
+export default {
+  props: {
+  },
+  setup(props) {
+    let active = ref(0);
+
+    let updateActive = (value) => (active.value = value);
+
+    provide("accordion", { active, updateActive });
+
+    return {
+    };
+  },
+};
+</script>
