1bf3a0d239d6f1d693ac51f92cf5561c7a6f0f28
maciejg-git <ender@localhost>
2021-09-12 00:20:52 +0200

    update docs

---
+++ src/components/Documentation/DocumentationDivider.vue
@@ -0,0 +1,205 @@
+<template>
+  <h3>Table</h3>
+  <p></p>
+
+  <section>
+    <h4>Reference</h4>
+    <p></p>
+    <v-table
+      :items="reference"
+      :definition="referenceDefinition"
+      style-table="fixed"
+      style-header-cell="bordered"
+      style-cell="bordered"
+    >
+      <template #cell:type="{ value }">
+        <code class="code-word mx-1">
+          {{ value }}
+        </code>
+      </template>
+      <template #cell:default="{ value }">
+        <code class="text-sm">{{ value }}</code>
+      </template>
+      <template #cell:description="{ value }">
+        <span v-html="value"></span>
+      </template>
+    </v-table>
+
+    <h6>Styling props</h6>
+    <p></p>
+    <v-table
+      :items="referenceStyles"
+      :definition="referenceStylesDefinition"
+      style-table="fixed"
+      style-header-cell="bordered"
+      style-cell="bordered"
+    >
+      <template #cell:description="{ value }"
+        ><span v-html="value"></span
+      ></template>
+    </v-table>
+  </section>
+
+  <section>
+    <h4>Example</h4>
+    <div class="example">
+      <p>
+        Lorem Ipsum is simply dummy text of the printing and typesetting
+            industry. Lorem Ipsum has been the industry's standard dummy text ever
+            since the 1500s, when an unknown printer took a galley of type and
+            scrambled it to make a type specimen book. It has survived not only
+            five centuries, but also the leap into electronic typesetting,
+            remaining essentially unchanged. It was popularised in the 1960s with
+            the release of Letraset sheets containing Lorem Ipsum passages, and
+            more recently with desktop publishing software like Aldus PageMaker
+            including versions of Lorem Ipsum.
+      </p>
+      <v-divider class="my-4" />
+      <p>
+        It is a long established fact that a reader will be distracted by the
+            readable content of a page when looking at its layout. The point of
+            using Lorem Ipsum is that it has a more-or-less normal distribution of
+            letters, as opposed to using 'Content here, content here', making it
+            look like readable English. Many desktop publishing packages and web
+            page editors now use Lorem Ipsum as their default model text, and a
+            search for 'lorem ipsum' will uncover many web sites still in their
+            infancy. Various versions have evolved over the years, sometimes by
+            accident, sometimes on purpose (injected humour and the like).
+      </p>
+    </div>
+    <pre>
+      <code>
+      </code>
+    </pre>
+  </section>
+</template>
+
+<script>
+import { ref, reactive, onMounted } from "vue";
+import hljs from "highlight.js";
+
+export default {
+  setup(props) {
+    let reference = ref([
+      {
+        prop: "name",
+        type: "Array",
+        default: "undefined",
+        description:
+          "Name of the component",
+      },
+    ]);
+
+    let referenceDefinition = ref([
+      {
+        key: "prop",
+        sortable: true,
+        class: () => "whitespace-nowrap",
+      },
+      {
+        key: "type",
+        sortable: true,
+      },
+      {
+        key: "default",
+        class: () => "whitespace-nowrap",
+      },
+      {
+        key: "description",
+      },
+    ]);
+
+    let referenceStyles = ref([
+      {
+        prop: "style-table",
+        description: "Main table element",
+      },
+    ]);
+
+    let referenceStylesDefinition = ref([
+      {
+        key: "prop",
+        class: () => "w-1 whitespace-nowrap",
+      },
+      {
+        key: "description",
+      },
+    ]);
+
+    let referenceEvents = ref([
+      {
+        event: "update:modelValue",
+        description: "Update v-model",
+      },
+    ]);
+
+    let referenceEventsDefinition = ref([
+      {
+        key: "event",
+        class: () => "w-1 whitespace-nowrap",
+      },
+      {
+        key: "description",
+      },
+    ]);
+
+    let referenceSlots = ref([
+      {
+        slot: "-",
+        description: "This component does not provide any slots.",
+      },
+    ]);
+
+    let referenceSlotsDefinition = ref([
+      {
+        key: "slot",
+        class: () => "w-1 whitespace-nowrap",
+      },
+      {
+        key: "description",
+      },
+    ]);
+
+    let referenceComponents = ref([
+      {
+        component: "-",
+        description: "This component does not provide any child components.",
+      },
+    ]);
+
+    let referenceComponentsDefinition = ref([
+      {
+        key: "component",
+        class: () => "w-1 whitespace-nowrap",
+      },
+      {
+        key: "description",
+      },
+    ]);
+
+    let example = reactive({});
+
+    onMounted(() => {
+      hljs.highlightAll();
+    });
+
+    return {
+      reference,
+      referenceDefinition,
+      referenceStyles,
+      referenceStylesDefinition,
+      referenceEvents,
+      referenceEventsDefinition,
+      referenceSlots,
+      referenceSlotsDefinition,
+      referenceComponents,
+      referenceComponentsDefinition,
+      example,
+    };
+  },
+};
+</script>
+
+<style scoped>
+@import "./Documentation.css";
+</style>
