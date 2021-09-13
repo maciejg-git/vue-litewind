d4c1c1decdf235c11cf1667264af26c68576ed00
maciejg-git <ender@localhost>
2021-09-14 01:41:03 +0200

    add state to icon

--- src/components/vIcon.vue
+++ src/components/vIcon.vue
@@ -1,11 +1,35 @@
 <template>
-  <component :is="icon + '-icon'"></component>
+  <component :is="icon + '-icon'" :class="classes"></component>
 </template>
 
 <script>
+import { computed, inject } from "vue";
+
 export default {
   props: {
     icon: { type: String, default: "" },
+    state: { type: String, default: "" },
+    stateIcons: { type: Object, default: () => {} },
+  },
+  setup(props) {
+    let stateIcons = inject("stateIcons");
+
+    let icon = computed(() => {
+      if (props.state) {
+        return stateIcons[props.state];
+      } else {
+        return props.icon;
+      }
+    });
+
+    let classes = computed(() => {
+      return props.state ? "b-icon--" + props.state + "-state" : "";
+    });
+
+    return {
+      icon,
+      classes,
+    };
   },
 };
 </script>
