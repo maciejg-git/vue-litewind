ac715de09d92a788da5258c092fd32856f0d46c2
maciejg-git <ender@localhost>
2021-09-14 23:55:29 +0200

    fix use click outside

--- src/components/composition/use-click-outside.js
+++ src/components/composition/use-click-outside.js
@@ -1,9 +1,14 @@
+import { unref, onUnmounted } from "vue"
+
 export default function useClickOutside() {
   function onClickOutside(elements, callback) {
     let clickOutside = function (ev) {
       if (!elements.length) return;
+
       for (let el of elements) {
-        if (el.value === ev.target || ev.composedPath().includes(el.value)) {
+        el = unref(el)
+        if (!el) return;
+        if (el === ev.target || ev.composedPath().includes(el)) {
           return;
         }
       }
@@ -11,6 +16,12 @@ export default function useClickOutside() {
     };
 
     document.body.addEventListener("mousedown", clickOutside);
+
+    let stop = () => {
+      removeEventListener("mousedown", clickOutside)
+    }
+
+    onUnmounted(stop)
   }
   return {
     onClickOutside,
