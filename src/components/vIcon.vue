c05f22f82ab1e986f032c4efb9252b6f7a5fe2fb
maciejg-git <ender@localhost>
2021-09-17 00:18:52 +0200

    add icon types

--- src/components/vIcon.vue
+++ src/components/vIcon.vue
@@ -4,6 +4,7 @@
 
 <script>
 import { computed, inject } from "vue";
+import { isObject, isString } from "../tools/tools";
 
 export default {
   props: {
@@ -15,18 +16,18 @@ export default {
 
     let icon = computed(() => {
       if (props.icon) return props.icon + "-icon";
+
       let type = iconTypes[props.iconType];
       if (!type) return;
-      let i = null;
-      if (typeof type === "object") {
-        if (type.icon) i = type.icon
-        else return
-      }
-      else {
-        if (typeof type === "string") i = type
-        else return
-      }
-      return i + "-icon";
+
+      let icon = null;
+
+      if (isObject(type)) icon = type.icon ? type.icon : null;
+      else if (isString(type)) icon = type;
+
+      if (!icon) return
+
+      return icon + "-icon";
     });
 
     let classes = computed(() => {
