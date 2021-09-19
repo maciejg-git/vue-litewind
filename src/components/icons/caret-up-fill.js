4d0bf073214fb7af8cab0907c9465f7226b2bf78
maciejg-git <ender@localhost>
2021-09-19 22:23:59 +0200

    add buildin icons

---
+++ src/components/icons/caret-up-fill.js
@@ -0,0 +1,12 @@
+import { h } from 'vue'
+export default {
+  name: "CaretUpFill",
+  vendor: "B",
+  render() {
+    return h(
+      "svg",
+      { "xmlns": "http://www.w3.org/2000/svg","width": "16","height": "16","fill": "currentColor","class": "b-icon","viewBox": "0 0 16 16" },
+      [ h("path", { "d": "m7.247 4.86-4.796 5.481c-.566.647-.106 1.659.753 1.659h9.592a1 1 0 0 0 .753-1.659l-4.796-5.48a1 1 0 0 0-1.506 0z" }) ]
+    )
+  }
+}
