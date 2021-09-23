49a57dfd7c9a4209e344fd8a7b9f66cde48cb410
maciejg-git <ender@localhost>
2021-09-24 01:44:55 +0200

    add button collapse

---
+++ src/components/vButtonCollapse.vue
@@ -0,0 +1,35 @@
+<template>
+  <v-button @click="toggle">
+    <span class="ml-auto">
+      <slot name="default"></slot>
+    </span>
+    <v-chevron
+      initial="down"
+      rotate180
+      counter-clockwise
+      :triangle="triangle"
+      :icon="icon"
+      :switch="modelValue"
+      class="ml-auto"
+    />
+  </v-button>
+</template>
+
+<script>
+export default {
+  props: {
+    modelValue: { type: Boolean, default: false },
+    triangle: { type: Boolean, default: false },
+    icon: { type: String, default: "" },
+  },
+  setup(props, { emit }) {
+    let toggle = () => {
+      emit("update:modelValue", !props.modelValue);
+    };
+
+    return {
+      toggle,
+    };
+  },
+};
+</script>
