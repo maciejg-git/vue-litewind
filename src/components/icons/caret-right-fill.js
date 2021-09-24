170fe454afd9ab0f2fb46d161f46cf29e71cb4e8
maciejg-git <ender@localhost>
2021-09-25 00:39:17 +0200

    add icons

--- src/components/icons/caret-right-fill.js
+++ src/components/icons/caret-right-fill.js
@@ -5,8 +5,7 @@ export default {
   render() {
     return h(
       "svg",
-      { "xmlns": "http://www.w3.org/2000/svg","width": "16","height": "16","fill": "currentColor","class": "b-icon","viewBox": "0 0 16 16" },
-      [ h("path", { "d": "m12.14 8.753-5.482 4.796c-.646.566-1.658.106-1.658-.753V3.204a1 1 0 0 1 1.659-.753l5.48 4.796a1 1 0 0 1 0 1.506z" }) ]
+      {"xmlns":"http://www.w3.org/2000/svg","width":"16","height":"16","fill":"currentColor","class":"b-icon","viewBox":"0 0 16 16","data-name":"b-caret-right-fill","innerHTML":"  <path d='m12.14 8.753-5.482 4.796c-.646.566-1.658.106-1.658-.753V3.204a1 1 0 0 1 1.659-.753l5.48 4.796a1 1 0 0 1 0 1.506z'/>"},
     )
   }
 }
