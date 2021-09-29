860fbfdc8d81e460ce6f8e9e6921be832194e283
maciejg-git <ender@localhost>
2021-09-30 01:42:32 +0200

    add examples

---
+++ src/components/Documentation/examples/ExampleModalSimple.vue
@@ -0,0 +1,26 @@
+<template>
+  <v-button @click="isVisible = !isVisible">Show modal</v-button>
+
+  <v-modal
+    v-model="isVisible"
+    title="Simple modal"
+    primary-button-close
+    secondary-button-close
+  >
+    Simple modal it is.
+  </v-modal>
+</template>
+
+<script>
+import { ref } from "vue";
+
+export default {
+  setup() {
+    let isVisible = ref(false);
+
+    return {
+      isVisible,
+    };
+  },
+};
+</script>
---
+++ src/components/Documentation/examples/ExampleTableSimple.vue
@@ -0,0 +1,22 @@
+<template>
+  <v-table :items="example.data" class="w-full"></v-table>
+</template>
+
+<script>
+import { reactive } from "vue";
+/* CUT START */
+import dataJSON from "../../../data.json";
+/* CUT END */
+
+export default {
+  setup() {
+    let example = reactive({
+      data: dataJSON.slice(0, 5),
+    });
+
+    return {
+      example,
+    };
+  },
+};
+</script>
