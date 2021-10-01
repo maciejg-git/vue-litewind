cec7a65c1d3eb46d3952c64ca3491e904d07b8ae
maciejg-git <ender@localhost>
2021-10-02 00:55:08 +0200

    update

---
+++ src/components/Documentation/examples/ExampleDropdownSimple.vue
@@ -0,0 +1,37 @@
+<template>
+  <v-dropdown auto-close-menu>
+    <template #activator>
+      <v-button-dropdown>
+        Dropdown menu
+      </v-button-dropdown>
+    </template>
+    <v-card width="320px" style-card="shadow">
+      <v-dropdown-menu-item tag="button">
+        Menu item
+      </v-dropdown-menu-item>
+      <v-dropdown-menu-item tag="button">
+        Second menu item
+      </v-dropdown-menu-item>
+      <v-dropdown-menu-item active tag="button">
+        Active menu item
+      </v-dropdown-menu-item>
+      <v-dropdown-menu-item tag="button">
+        Active menu item
+      </v-dropdown-menu-item>
+    </v-card>
+  </v-dropdown>
+</template>
+
+<script>
+import { ref, reactive } from "vue";
+
+export default {
+  setup() {
+    let example = reactive({
+    });
+
+    return {
+    };
+  },
+};
+</script>
