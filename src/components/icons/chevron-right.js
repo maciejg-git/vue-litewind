2ba2f5df050467a93b0e89d10248199c02cc095b
maciejg-git <ender@localhost>
2022-02-05 01:45:28 +0100

    add icons

---
+++ src/components/icons/chevron-right.js
@@ -0,0 +1,15 @@
+import { h } from 'vue'
+export default {
+  $_icon: {
+    name: "ChevronRight",
+    vendor: "B",
+    type: "",
+    tags: ["chevron","right"],
+  },
+  render() {
+    return h(
+      "svg",
+      {"xmlns":"http://www.w3.org/2000/svg","width":"16","height":"16","fill":"currentColor","class":"v-icon","viewBox":"0 0 16 16","data-name":"b-chevron-right","innerHTML":"  <path fill-rule='evenodd' d='M4.646 1.646a.5.5 0 0 1 .708 0l6 6a.5.5 0 0 1 0 .708l-6 6a.5.5 0 0 1-.708-.708L10.293 8 4.646 2.354a.5.5 0 0 1 0-.708z'/>"},
+    )
+  }
+}
