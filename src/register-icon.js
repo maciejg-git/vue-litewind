4c81a6ca3c58968fd5acbc961d84b4f2650539f7
maciejg-git <ender@localhost>
2022-06-06 00:43:52 +0200

    add string option

--- src/register-icon.js
+++ src/register-icon.js
@@ -1,15 +1,23 @@
 export default function registerIcon(app, icons, options) {
   let vendorPrefix =
-    options && options.vendorPrefix !== undefined
-      ? options.vendorPrefix
-      : true;
+    options && options.vendorPrefix !== undefined ? options.vendorPrefix : true;
 
   let iconSuffix =
     options && options.iconSufix !== undefined ? options.iconSufix : false;
 
-  icons.forEach((i) => {
-    let vendor = vendorPrefix ? i.$_icon.vendor : ""
-    let suffix = iconSuffix ? "Icon" : "";
-    app.component(`${vendor}${i.$_icon.name}${i.$_icon.type}${suffix}`, i);
-  });
+  if (Array.isArray(icons)) {
+    icons.forEach((i) => {
+      let vendor = vendorPrefix ? i.$_icon.vendor : "";
+      let suffix = iconSuffix ? "Icon" : "";
+      app.component(`${vendor}${i.$_icon.name}${i.$_icon.type}${suffix}`, i);
+    });
+    return;
+  }
+
+  let vendor = vendorPrefix ? icons.$_icon.vendor : "";
+  let suffix = iconSuffix ? "Icon" : "";
+  app.component(
+    `${vendor}${icons.$_icon.name}${icons.$_icon.type}${suffix}`,
+    icons
+  );
 }
