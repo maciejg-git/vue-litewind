b4d453e79623a63e90a335e1e889e8f680301d7c
maciejg-git <ender@localhost>
2022-08-18 19:38:00 +0200

    update

--- vite-lib.config.js
+++ vite-lib.config.js
@@ -6,7 +6,7 @@ export default defineConfig({
   plugins: [vue()],
   build: {
     lib: {
-      entry: path.resolve(__dirname, "src/components/index.js"),
+      entry: path.resolve(__dirname, "src/index.js"),
       name: "MyLib",
     },
     rollupOptions: {
