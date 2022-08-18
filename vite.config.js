c4b56d7d5623e0e333290a9a85644f614cf450b5
maciejg-git <ender@localhost>
2022-08-18 20:33:24 +0200

    update

---
+++ vite.config.js
@@ -0,0 +1,25 @@
+import { resolve } from 'path'
+import { defineConfig } from 'vite'
+
+export default defineConfig({
+  build: {
+    lib: {
+      entry: resolve(__dirname, 'src/index.js'),
+      name: 'vue-component-tailwind',
+      // the proper extensions will be added
+      fileName: 'vue-component-tailwind'
+    },
+    rollupOptions: {
+      // make sure to externalize deps that shouldn't be bundled
+      // into your library
+      external: ['vue'],
+      output: {
+        // Provide global variables to use in the UMD build
+        // for externalized deps
+        globals: {
+          vue: 'Vue'
+        }
+      }
+    }
+  }
+})
