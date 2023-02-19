8445e46a1f686afaeb43af648d5329cd829a6b13
maciejg-git <ender@localhost>
2023-02-19 16:39:56 +0100

    update tests

--- src/test/Table.test.js
+++ src/test/Table.test.js
@@ -1,31 +1,41 @@
-import { render, fireEvent, prettyDOM } from "@testing-library/vue";
-import "@testing-library/jest-dom"
+import { render, fireEvent, prettyDOM, within } from "@testing-library/vue";
+import "@testing-library/jest-dom";
 import Table from "../components/vTable.vue";
+import data from "./data/company-simple.json";
 
-let data = [
-  { id: 1, name: "name", city: "city" },
-  { id: 2, name: "name2", city: "city2" },
-  { id: 3, name: "name3", city: "city3" },
-  { id: 4, name: "name4", city: "city4" },
-  { id: 5, name: "name5", city: "city5" },
-]
+// let data = [
+//   { id: 1, name: "name", city: "city" },
+//   { id: 2, name: "name2", city: "city2" },
+//   { id: 3, name: "name3", city: "city3" },
+//   { id: 4, name: "name4", city: "city4" },
+//   { id: 5, name: "name5", city: "city5" },
+// ]
 
 let props = {
   items: data,
   primaryKey: "id",
-}
+};
 
 let definition = [
   {
     key: "id",
   },
   {
-    key: "name",
+    key: "first_name",
+  },
+  {
+    key: "last_name",
   },
   {
     key: "city",
   },
-]
+  {
+    key: "department",
+  },
+  {
+    key: "title",
+  },
+];
 
 afterEach(() => {
   definition = [
@@ -33,13 +43,22 @@ afterEach(() => {
       key: "id",
     },
     {
-      key: "name",
+      key: "first_name",
+    },
+    {
+      key: "last_name",
     },
     {
       key: "city",
     },
-  ]
-})
+    {
+      key: "department",
+    },
+    {
+      key: "title",
+    },
+  ];
+});
 
 test("renders component", () => {
   const { getByRole } = render(Table, {
@@ -48,273 +67,18 @@ test("renders component", () => {
     },
   });
 
-  expect(getByRole("table")).toMatchInlineSnapshot(`
-    <table
-      class="table"
-      data-v-0d7422a8=""
-    >
-      <!--v-if-->
-      <thead
-        class="table__header-row"
-        data-v-0d7422a8=""
-      >
-        <tr
-          data-v-0d7422a8=""
-        >
-          
-          
-          <th
-            class="table__header-cell"
-            data-v-0d7422a8=""
-          >
-            <div
-              class="flex items-center"
-              data-v-0d7422a8=""
-            >
-              Id 
-              <!--v-if-->
-            </div>
-          </th>
-          
-          
-          <th
-            class="table__header-cell"
-            data-v-0d7422a8=""
-          >
-            <div
-              class="flex items-center"
-              data-v-0d7422a8=""
-            >
-              Name 
-              <!--v-if-->
-            </div>
-          </th>
-          
-          
-          <th
-            class="table__header-cell"
-            data-v-0d7422a8=""
-          >
-            <div
-              class="flex items-center"
-              data-v-0d7422a8=""
-            >
-              City 
-              <!--v-if-->
-            </div>
-          </th>
-          
-          
-        </tr>
-      </thead>
-      <tbody
-        data-v-0d7422a8=""
-      >
-        
-        
-        <tr
-          class="table__row"
-          data-v-0d7422a8=""
-        >
-          
-          
-          <td
-            class="table__cell"
-            data-v-0d7422a8=""
-          >
-            
-            1
-            
-          </td>
-          
-          
-          <td
-            class="table__cell"
-            data-v-0d7422a8=""
-          >
-            
-            name
-            
-          </td>
-          
-          
-          <td
-            class="table__cell"
-            data-v-0d7422a8=""
-          >
-            
-            city
-            
-          </td>
-          
-          
-        </tr>
-        <!--v-if-->
-        
-        
-        <tr
-          class="table__row"
-          data-v-0d7422a8=""
-        >
-          
-          
-          <td
-            class="table__cell"
-            data-v-0d7422a8=""
-          >
-            
-            2
-            
-          </td>
-          
-          
-          <td
-            class="table__cell"
-            data-v-0d7422a8=""
-          >
-            
-            name2
-            
-          </td>
-          
-          
-          <td
-            class="table__cell"
-            data-v-0d7422a8=""
-          >
-            
-            city2
-            
-          </td>
-          
-          
-        </tr>
-        <!--v-if-->
-        
-        
-        <tr
-          class="table__row"
-          data-v-0d7422a8=""
-        >
-          
-          
-          <td
-            class="table__cell"
-            data-v-0d7422a8=""
-          >
-            
-            3
-            
-          </td>
-          
-          
-          <td
-            class="table__cell"
-            data-v-0d7422a8=""
-          >
-            
-            name3
-            
-          </td>
-          
-          
-          <td
-            class="table__cell"
-            data-v-0d7422a8=""
-          >
-            
-            city3
-            
-          </td>
-          
-          
-        </tr>
-        <!--v-if-->
-        
-        
-        <tr
-          class="table__row"
-          data-v-0d7422a8=""
-        >
-          
-          
-          <td
-            class="table__cell"
-            data-v-0d7422a8=""
-          >
-            
-            4
-            
-          </td>
-          
-          
-          <td
-            class="table__cell"
-            data-v-0d7422a8=""
-          >
-            
-            name4
-            
-          </td>
-          
-          
-          <td
-            class="table__cell"
-            data-v-0d7422a8=""
-          >
-            
-            city4
-            
-          </td>
-          
-          
-        </tr>
-        <!--v-if-->
-        
-        
-        <tr
-          class="table__row"
-          data-v-0d7422a8=""
-        >
-          
-          
-          <td
-            class="table__cell"
-            data-v-0d7422a8=""
-          >
-            
-            5
-            
-          </td>
-          
-          
-          <td
-            class="table__cell"
-            data-v-0d7422a8=""
-          >
-            
-            name5
-            
-          </td>
-          
-          
-          <td
-            class="table__cell"
-            data-v-0d7422a8=""
-          >
-            
-            city5
-            
-          </td>
-          
-          
-        </tr>
-        <!--v-if-->
-        
-        
-      </tbody>
-    </table>
-  `);
+  expect(getByRole("table")).toMatchSnapshot();
+});
+
+test("renders component without definition prop", () => {
+  const { getByRole, getByText, getAllByRole } = render(Table, {
+    props: {
+      ...props,
+    },
+  });
+
+  expect(getAllByRole("row")).toHaveLength(51);
+  expect(getAllByRole("cell")).toHaveLength(300);
 });
 
 test("renders component with definition prop", () => {
@@ -325,66 +89,200 @@ test("renders component with definition prop", () => {
     },
   });
 
-  expect(getAllByRole("row")).toHaveLength(6);
-  expect(getAllByRole("cell")).toHaveLength(15);
-  expect(getByRole("cell", { name: "name" })).toBeInTheDocument();
+  expect(getAllByRole("row")).toHaveLength(51);
+  expect(getAllByRole("cell")).toHaveLength(300);
 });
 
-test("reorders columns based on definition", () => {
-  definition.splice(2, 0, definition.splice(1, 1)[0])
+describe("correctly handles definition prop", () => {
+  test("reorders columns based on definition", () => {
+    definition.splice(5, 0, definition.splice(4, 1)[0]);
 
-  const { getAllByRole } = render(Table, {
+    const { getAllByRole } = render(Table, {
+      props: {
+        ...props,
+        definition,
+      },
+    });
+
+    let headers = getAllByRole("columnheader");
+
+    expect(headers).toHaveLength(6);
+    expect(headers[0]).toHaveTextContent("Id");
+    expect(headers[1]).toHaveTextContent("First Name");
+    expect(headers[2]).toHaveTextContent("Last Name");
+    expect(headers[3]).toHaveTextContent("City");
+    expect(headers[4]).toHaveTextContent("Title");
+    expect(headers[5]).toHaveTextContent("Department");
+  });
+
+  test("correctly names column headers from definition", () => {
+    definition[0].label = "label";
+    definition[1].label = "label2";
+    definition[2].label = "label3";
+
+    const { getAllByRole } = render(Table, {
+      props: {
+        ...props,
+        definition,
+      },
+    });
+
+    let headers = getAllByRole("columnheader");
+
+    expect(headers).toHaveLength(6);
+    expect(headers[0]).toHaveTextContent("label");
+    expect(headers[1]).toHaveTextContent("label2");
+    expect(headers[2]).toHaveTextContent("label3");
+  });
+
+  test("correctly adds new columns not defined in items", () => {
+    definition[6] = {
+      key: "edit",
+    };
+
+    const { getAllByRole } = render(Table, {
+      props: {
+        ...props,
+        definition,
+      },
+    });
+
+    let headers = getAllByRole("columnheader");
+
+    expect(headers).toHaveLength(7);
+    expect(headers[0]).toHaveTextContent("Id");
+    expect(headers[1]).toHaveTextContent("First Name");
+    expect(headers[2]).toHaveTextContent("Last Name");
+    expect(headers[3]).toHaveTextContent("City");
+    expect(headers[4]).toHaveTextContent("Department");
+    expect(headers[5]).toHaveTextContent("Title");
+    expect(headers[6]).toHaveTextContent("Edit");
+  });
+
+  test("correctly excludes columns not defined in definition", () => {
+    definition.splice(4, 2);
+
+    const { getAllByRole } = render(Table, {
+      props: {
+        ...props,
+        definition,
+      },
+    });
+
+    let headers = getAllByRole("columnheader");
+
+    expect(headers).toHaveLength(4);
+    expect(headers[0]).toHaveTextContent("Id");
+    expect(headers[1]).toHaveTextContent("First Name");
+    expect(headers[2]).toHaveTextContent("Last Name");
+    expect(headers[3]).toHaveTextContent("City");
+  });
+
+  test("correctly hides columns (visible property)", () => {
+    definition[4].visible = false;
+    definition[5].visible = false;
+
+    const { getAllByRole } = render(Table, {
+      props: {
+        ...props,
+        definition,
+      },
+    });
+
+    let headers = getAllByRole("columnheader");
+
+    expect(headers).toHaveLength(4);
+    expect(headers[0]).toHaveTextContent("Id");
+    expect(headers[1]).toHaveTextContent("First Name");
+    expect(headers[2]).toHaveTextContent("Last Name");
+    expect(headers[3]).toHaveTextContent("City");
+  });
+
+  test("correctly disables filtering for columns (filterable property)", () => {
+    definition[0].filterable = false;
+    definition[1].filterable = false;
+    definition[2].filterable = false;
+    definition[3].filterable = false;
+    definition[5].filterable = false;
+
+    const { getAllByRole } = render(Table, {
+      props: {
+        ...props,
+        definition,
+        filter: "de",
+      },
+    });
+
+    expect(getAllByRole("row")).toHaveLength(10);
+  });
+});
+
+test("correctly filters items (filter prop)", () => {
+  const { getAllByRole, getAllByText } = render(Table, {
     props: {
       ...props,
-      definition,
+      filter: "de",
     },
   });
 
-  let headers = getAllByRole("columnheader")
-
-  expect(headers).toHaveLength(3)
-  expect(headers[0]).toHaveTextContent("Id")
-  expect(headers[1]).toHaveTextContent("City")
-  expect(headers[2]).toHaveTextContent("Name")
+  expect(getAllByRole("row")).toHaveLength(17);
+  expect(getAllByText(/[Dd]e/)).toHaveLength(21);
 });
 
-test("correctly names column headers from definition", () => {
-  definition[0].label = "label"
-  definition[1].label = "label2"
-  definition[2].label = "label3"
-
-  const { getAllByRole } = render(Table, {
+test("renders caption (caption slot)", () => {
+  const { getByText } = render(Table, {
     props: {
       ...props,
-      definition,
+    },
+    slots: {
+      caption: "caption",
     },
   });
 
-  let headers = getAllByRole("columnheader")
-
-  expect(headers).toHaveLength(3)
-  expect(headers[0]).toHaveTextContent("label")
-  expect(headers[1]).toHaveTextContent("label2")
-  expect(headers[2]).toHaveTextContent("label3")
+  expect(getByText("caption")).toBeInTheDocument();
 });
 
-test("correctly adds new columns not defined in items", () => {
-  definition[3] = {
-    key: "edit"
-  }
-
+test("renders correct number of items per page", () => {
   const { getAllByRole } = render(Table, {
     props: {
       ...props,
-      definition,
+      itemsPerPage: 10,
     },
   });
 
-  let headers = getAllByRole("columnheader")
+  expect(getAllByRole("row")).toHaveLength(11);
+});
 
-  expect(headers).toHaveLength(4)
-  expect(headers[0]).toHaveTextContent("Id")
-  expect(headers[1]).toHaveTextContent("Name")
-  expect(headers[2]).toHaveTextContent("City")
-  expect(headers[3]).toHaveTextContent("Edit")
+test("correctly changes page (page prop)", async () => {
+  const { getAllByRole, getByText, rerender } = render(Table, {
+    props: {
+      ...props,
+      itemsPerPage: 20,
+      page: 1,
+    },
+  });
+
+  let rows = getAllByRole("row");
+  expect(rows).toHaveLength(21);
+  expect(within(rows[1]).getAllByRole("cell")[0]).toHaveTextContent("1");
+
+  await rerender({
+    ...props,
+    itemsPerPage: 20,
+    page: 2,
+  });
+
+  rows = getAllByRole("row");
+  expect(rows).toHaveLength(21);
+  expect(within(rows[1]).getAllByRole("cell")[0]).toHaveTextContent("21");
+
+  await rerender({
+    ...props,
+    itemsPerPage: 20,
+    page: 3,
+  });
+
+  rows = getAllByRole("row");
+  expect(rows).toHaveLength(11);
+  expect(within(rows[1]).getAllByRole("cell")[0]).toHaveTextContent("41");
 });
