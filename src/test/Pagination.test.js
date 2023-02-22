3690ed17761202c68150b5d22bda32627e741b02
maciejg-git <ender@localhost>
2023-02-23 00:15:02 +0100

    update tests

--- src/test/Pagination.test.js
+++ src/test/Pagination.test.js
@@ -1,4 +1,5 @@
 import { render, fireEvent } from "@testing-library/vue";
+import "@testing-library/jest-dom"
 import Pagination from "../components/vPagination.vue";
 
 test("renders buttons", () => {
@@ -6,19 +7,41 @@ test("renders buttons", () => {
     props: {},
   });
 
-  getByRole("button", { name: "Previous page" });
-  getByRole("button", { name: "Next page" });
+  expect(getByRole("button", { name: "Previous page" })).toBeInTheDocument();
+  expect(getByRole("button", { name: "Next page" })).toBeInTheDocument();
 });
 
-test("renders buttons", () => {
+test("renders page number buttons", () => {
   const { getByRole } = render(Pagination, {
     props: {
-      itemsCount: 80,
+      itemsCount: 40,
       itemsPerPage: 10,
+      maxPages: 100,
+    },
+  });
+
+  expect(getByRole("button", { name: "Page 1" })).toBeInTheDocument();
+  expect(getByRole("button", { name: "Page 2" })).toBeInTheDocument();
+  expect(getByRole("button", { name: "Page 3" })).toBeInTheDocument();
+  expect(getByRole("button", { name: "Page 4" })).toBeInTheDocument();
+});
+
+test("renders correct page number buttons", () => {
+  const { getByRole, queryByRole, getByText } = render(Pagination, {
+    props: {
+      itemsCount: 40,
+      itemsPerPage: 5,
       maxPages: 5,
     },
   });
 
-  getByRole("button", { name: "Page 1" });
-  getByRole("button", { name: "Page 8" });
+    expect(getByRole("button", { name: "Page 1" })).toBeInTheDocument();
+    expect(getByRole("button", { name: "Page 2" })).toBeInTheDocument();
+    expect(getByRole("button", { name: "Page 3" })).toBeInTheDocument();
+    expect(getByRole("button", { name: "Page 8" })).toBeInTheDocument();
+    expect(getByText("...")).toBeInTheDocument();
+    expect(queryByRole("button", { name: "Page 4" })).not.toBeInTheDocument();
+    expect(queryByRole("button", { name: "Page 5" })).not.toBeInTheDocument();
+    expect(queryByRole("button", { name: "Page 6" })).not.toBeInTheDocument();
+    expect(queryByRole("button", { name: "Page 7" })).not.toBeInTheDocument();
 });
---
+++ src/test/Sidepanel.test.js
@@ -0,0 +1,115 @@
+import { render, fireEvent, prettyDOM } from "@testing-library/vue";
+import "@testing-library/jest-dom"
+import Sidepanel from "../components/vSidepanel.vue";
+
+test("renders component (hidden)", () => {
+  const { getByRole, getByText } = render(Sidepanel, {
+    props: {},
+  });
+
+  expect(getByRole("complementary", { hidden: true })).toBeInTheDocument();
+});
+
+test("renders component (open)", () => {
+  const { getByRole, getByText } = render(Sidepanel, {
+    props: {
+      modelValue: true,
+    },
+  });
+
+  expect(getByRole("complementary")).toBeInTheDocument();
+});
+
+describe("close button", () => {
+  test("renders close button by default", () => {
+    const { getByRole, container } = render(Sidepanel, {
+      props: {
+        modelValue: true,
+      },
+    });
+
+    expect(getByRole("complementary")).toBeInTheDocument();
+    expect(container.querySelector("svg")).toBeInTheDocument();
+  });
+
+  test("should not render close button (show-close-button prop)", () => {
+    const { getByRole, container } = render(Sidepanel, {
+      props: {
+        modelValue: true,
+        showCloseButton: false,
+      },
+    });
+
+    expect(getByRole("complementary")).toBeInTheDocument();
+    expect(container.querySelector("svg")).not.toBeInTheDocument();
+  });
+})
+
+describe("header", () => {
+  test("renders header slot", () => {
+    const { getByRole, getByText, container } = render(Sidepanel, {
+      props: {
+        modelValue: true,
+      },
+      slots: {
+        header: "<h2>header</h2>"
+      }
+    });
+
+    expect(getByRole("complementary")).toBeInTheDocument();
+    expect(getByRole("heading")).toBeInTheDocument();
+    expect(container.querySelector("svg")).toBeInTheDocument();
+  });
+
+  test("should not render header slot (no-header prop)", () => {
+    const { getByRole, getByText, container, queryByRole } = render(Sidepanel, {
+      props: {
+        modelValue: true,
+        noHeader: true,
+      },
+      slots: {
+        header: "<h2>header</h2>"
+      }
+    });
+
+    expect(getByRole("complementary")).toBeInTheDocument();
+    expect(queryByRole("heading")).not.toBeInTheDocument();
+    expect(container.querySelector("svg")).not.toBeInTheDocument();
+  });
+})
+
+describe("left or right sidepanel", () => {
+  test("renders sidepanel on the right", () => {
+    const { getByRole, getByText } = render(Sidepanel, {
+      props: {
+        modelValue: true,
+      },
+    });
+
+    expect(getByRole("complementary")).toHaveClass("right-0");
+  });
+
+  test("renders sidepanel on the left", () => {
+    const { getByRole, getByText } = render(Sidepanel, {
+      props: {
+        modelValue: true,
+        sidebarLeft: true,
+      },
+    });
+
+    expect(getByRole("complementary")).toHaveClass("left-0");
+  });
+})
+
+test("close button should close sidepanel", async () => {
+  const { getByRole, getByText, queryByRole } = render(Sidepanel, {
+    props: {
+      modelValue: true,
+    },
+  });
+
+  expect(getByRole("complementary")).toBeInTheDocument();
+  let button = getByRole("button", { name: "Close" })
+  await fireEvent.click(button)
+  expect(queryByRole("complementary")).not.toBeInTheDocument();
+});
