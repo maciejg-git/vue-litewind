795fd1ed84224cdb0e2e40723261bb9a28dfdb2a
maciejg-git <ender@localhost>
2023-03-03 23:41:31 +0100

    update tests

--- src/test/DatePicker.test.js
+++ src/test/DatePicker.test.js
@@ -1,203 +1,58 @@
 import { render, fireEvent, prettyDOM } from "@testing-library/vue";
-import "@testing-library/jest-dom"
-import DatePicker from "../components/vDatePicker.vue";
-
-let pad = (d) => (d < 10 ? "0" + d : d);
-
-let date = new Date()
-date.setHours(0, 0, 0, 0)
-
-let today = date.toDateString().split(" ")
-let todayTest = `${date[1]} ${date[3]}`
-let todayArray = [date.getFullYear(), pad(date.getMonth() + 1), pad(date.getDate())]
-
-let exampleDate = [todayArray[0], todayArray[1], "01"].join("-")
-let exampleDateFrom = [todayArray[0], todayArray[1], "01"].join("-")
-let exampleDateTo = [todayArray[0], todayArray[1], "18"].join("-")
-
-date = new Date()
-
-let nextMonth = new Date(date.setMonth(date.getMonth() + 1)).toDateString().split(" ")
-let nextMonthTest = `${nextMonth[1]} ${nextMonth[3]}`
-
-date = new Date()
-
-let prevMonth = new Date(date.setMonth(date.getMonth() - 1)).toDateString().split(" ")
-let prevMonthTest = `${prevMonth[1]} ${prevMonth[3]}`
-
-date = new Date()
-
-let nextYear = new Date(date.setFullYear(date.getFullYear() + 1)).toDateString().split(" ")
-let nextYearTest = `${nextYear[1]} ${nextYear[3]}`
-
-date = new Date()
-
-let prevYear = new Date(date.setFullYear(date.getFullYear() - 1)).toDateString().split(" ")
-let prevYearTest = `${prevYear[1]} ${prevYear[3]}`
+import "@testing-library/jest-dom";
+import Checkbox from "../components/vCheckbox.vue";
 
 test("renders component", () => {
-  const { getByRole, getByText } = render(DatePicker, {
+  const { getByRole } = render(Checkbox, {
     props: {},
   });
 
-  getByRole("button", { name: "Previous year" });
-  getByRole("button", { name: "Next year" });
-  getByRole("button", { name: "Previous month" });
-  getByRole("button", { name: "Next month" });
-  getByRole("button", { name: "1" });
-  getByText("Mon")
-  getByText("Tue")
+  expect(getByRole("checkbox")).toBeInTheDocument();
 });
 
-test("previous month", async () => {
-  const { getByRole, findByText } = render(DatePicker, {
-    props: {},
-  });
-
-  const button = getByRole("button", { name: "Previous month" });
-  await fireEvent.click(button);
-  await findByText(prevMonthTest);
-});
-
-test("next month", async () => {
-  const { getByRole, findByText } = render(DatePicker, {
-    props: {},
-  });
-
-  const button = getByRole("button", { name: "Next month" });
-  await fireEvent.click(button);
-  await findByText(nextMonthTest);
-});
-
-test("previous year", async () => {
-  const { getByRole, findByText } = render(DatePicker, {
-    props: {},
-  });
-
-  const button = getByRole("button", { name: "Previous year" });
-  await fireEvent.click(button);
-  await findByText(prevYearTest);
-});
-
-test("next year", async () => {
-  const { getByRole, findByText } = render(DatePicker, {
-    props: {},
-  });
-
-  const button = getByRole("button", { name: "Next year" });
-  await fireEvent.click(button);
-  await findByText(nextYearTest);
-});
-
-test("renders buttons (buttons prop)", async () => {
-  const { getByRole } = render(DatePicker, {
+test("renders component with label", () => {
+  const { getByRole, getByText } = render(Checkbox, {
     props: {
-      buttons: true,
+      label: "username",
     },
   });
 
-  getByRole("button", { name: "OK" });
-  getByRole("button", { name: "Cancel" });
+  expect(getByRole("checkbox")).toBeInTheDocument();
+  expect(getByText("username")).toBeInTheDocument();
 });
 
-test("renders buttons (primary-button-label, secondary-button-label props)", async () => {
-  const { getByRole } = render(DatePicker, {
+describe("checked and unchecked", () => {
+  test("should be checked (v-model)", () => {
+    const { getByRole } = render(Checkbox, {
+      props: {
+        modelValue: true,
+      },
+    });
+
+    expect(getByRole("checkbox", { checked: true })).toBeInTheDocument();
+  });
+
+  test("should be unchecked (v-model)", () => {
+    const { getByRole } = render(Checkbox, {
+      props: {
+        modelValue: false,
+      },
+    });
+
+    expect(getByRole("checkbox", { checked: false })).toBeInTheDocument();
+  });
+});
+
+test("clicking label should check", async () => {
+  const { getByRole, getByText } = render(Checkbox, {
     props: {
-      buttons: true,
-      primaryButtonLabel: "primary button",
-      secondaryButtonLabel: "secondary button",
+      label: "username",
+      modelValue: false,
     },
   });
 
-  getByRole("button", { name: "primary button" });
-  getByRole("button", { name: "secondary button" });
-});
-
-test("renders localized names", async () => {
-  const { getByText } = render(DatePicker, {
-    props: {
-      locale: "pl-PL",
-    },
-  });
-
-  getByText("pon.")
-  getByText("wt.")
-});
-
-test("week starts on monday (monday-first-weekday prop)", async () => {
-  const { getAllByTestId, rerender } = render(DatePicker, {
-    props: {
-      mondayFirstWeekday: false,
-    },
-  });
-
-  let week = getAllByTestId("weekday")
-  expect(week[0]).toHaveTextContent("Sun")
-
-  await rerender({
-    mondayFirstWeekday: true,
-  })
-
-  week = getAllByTestId("weekday")
-  expect(week[0]).toHaveTextContent("Mon")
-});
-
-test("today is rendered", async () => {
-  const { getByTestId } = render(DatePicker, {
-    props: {},
-  });
-
-  expect(getByTestId("today")).toBeInTheDocument()
-});
-
-test("update v-model after selecting date (single mode)", async () => {
-  const { getByRole, emitted } = render(DatePicker, {
-    props: {},
-  });
-  
-  fireEvent.click(getByRole("button", { name: "1" }))
-
-  expect(emitted()).toHaveProperty("update:modelValue")
-  let modelValue = emitted('update:modelValue')
-  expect(modelValue[0][0]).toMatch(exampleDate)
-});
-
-test("update v-model prop (single mode)", async () => {
-  const { getByRole, getByText, emitted } = render(DatePicker, {
-    props: {
-      modelValue: exampleDate,
-    },
-  });
-  
-  expect(getByRole("button", { name: "1" }).className).toMatch(/selected/)
-});
-
-test("update v-model after selecting date (range mode)", async () => {
-  const { getByRole, emitted } = render(DatePicker, {
-    props: {
-      range: true,
-    },
-  });
-  
-  fireEvent.click(getByRole("button", { name: "1" }))
-  fireEvent.click(getByRole("button", { name: "18" }))
-
-  expect(emitted()).toHaveProperty("update:modelValue")
-  let modelValue = emitted('update:modelValue')
-  expect(Array.isArray(modelValue[0][0])).toBe(true)
-  expect(modelValue[0][0][0]).toMatch(exampleDateFrom)
-  expect(modelValue[0][0][1]).toMatch(exampleDateTo)
-});
-
-test("update v-model prop (range mode mode)", async () => {
-  const { getByRole } = render(DatePicker, {
-    props: {
-      modelValue: [exampleDateFrom, exampleDateTo],
-      range: true,
-    },
-  });
-  
-  Array(18).fill().map((v,i)=> i + 1).forEach((day) => {
-    expect(getByRole("button", { name: day }).className).toMatch(/selected/)
-  })
+  expect(getByText("username")).toBeInTheDocument();
+  expect(getByRole("checkbox", { checked: false })).toBeInTheDocument();
+  await fireEvent.click(getByText("username"))
+  expect(getByRole("checkbox", { checked: true })).toBeInTheDocument();
 });
--- src/test/Pagination.test.js
+++ src/test/Pagination.test.js
@@ -1,5 +1,5 @@
 import { render, fireEvent } from "@testing-library/vue";
-import "@testing-library/jest-dom"
+import "@testing-library/jest-dom";
 import Pagination from "../components/vPagination.vue";
 
 test("renders buttons", () => {
@@ -35,13 +35,166 @@ test("renders correct page number buttons", () => {
     },
   });
 
-    expect(getByRole("button", { name: "Page 1" })).toBeInTheDocument();
-    expect(getByRole("button", { name: "Page 2" })).toBeInTheDocument();
-    expect(getByRole("button", { name: "Page 3" })).toBeInTheDocument();
-    expect(getByRole("button", { name: "Page 8" })).toBeInTheDocument();
-    expect(getByText("...")).toBeInTheDocument();
-    expect(queryByRole("button", { name: "Page 4" })).not.toBeInTheDocument();
-    expect(queryByRole("button", { name: "Page 5" })).not.toBeInTheDocument();
-    expect(queryByRole("button", { name: "Page 6" })).not.toBeInTheDocument();
-    expect(queryByRole("button", { name: "Page 7" })).not.toBeInTheDocument();
+  expect(getByRole("button", { name: "Page 1" })).toBeInTheDocument();
+  expect(getByRole("button", { name: "Page 2" })).toBeInTheDocument();
+  expect(getByRole("button", { name: "Page 3" })).toBeInTheDocument();
+  expect(getByRole("button", { name: "Page 8" })).toBeInTheDocument();
+  expect(getByText("...")).toBeInTheDocument();
+  expect(queryByRole("button", { name: "Page 4" })).not.toBeInTheDocument();
+  expect(queryByRole("button", { name: "Page 5" })).not.toBeInTheDocument();
+  expect(queryByRole("button", { name: "Page 6" })).not.toBeInTheDocument();
+  expect(queryByRole("button", { name: "Page 7" })).not.toBeInTheDocument();
+});
+
+// click next prev
+// click page
+// no more than max pages
+// at least 3 pages
+
+test("should make first page active by default", async () => {
+  const { getByRole } = render(Pagination, {
+    props: {
+      itemsCount: 40,
+      itemsPerPage: 5,
+      maxPages: 5,
+    },
+  });
+
+  expect(
+    getByRole("button", { name: "Page 1", current: true })
+  ).toBeInTheDocument();
+});
+
+test("should use v-model to mage page active", async () => {
+  const { getByRole } = render(Pagination, {
+    props: {
+      itemsCount: 40,
+      itemsPerPage: 5,
+      maxPages: 5,
+      modelValue: 2,
+    },
+  });
+
+  expect(
+    getByRole("button", { name: "Page 2", current: true })
+  ).toBeInTheDocument();
+});
+
+describe("max pages", () => {
+  test("should not render more than max-pages pages", async () => {
+    const { queryAllByRole, getByText } = render(Pagination, {
+      props: {
+        itemsCount: 40,
+        itemsPerPage: 5,
+        maxPages: 5,
+      },
+    });
+
+    expect(queryAllByRole("button", { name: /Page/ })).toHaveLength(5);
+  });
+
+  test("should not render less than 3 pages", async () => {
+    const { queryAllByRole } = render(Pagination, {
+      props: {
+        itemsCount: 40,
+        itemsPerPage: 5,
+        maxPages: 1,
+      },
+    });
+
+    expect(queryAllByRole("button", { name: /Page/ })).toHaveLength(3);
+  });
+});
+
+describe("next and previous buttons", () => {
+  test("click next should change page to next", async () => {
+    const { getByRole } = render(Pagination, {
+      props: {
+        itemsCount: 40,
+        itemsPerPage: 5,
+        maxPages: 5,
+      },
+    });
+
+    expect(
+      getByRole("button", { name: "Page 1", current: true })
+    ).toBeInTheDocument();
+    await fireEvent.click(getByRole("button", { name: "Next page" }));
+    expect(
+      getByRole("button", { name: "Page 2", current: true })
+    ).toBeInTheDocument();
+  });
+
+  test("click previous should change page to previous", async () => {
+    const { getByRole } = render(Pagination, {
+      props: {
+        itemsCount: 40,
+        itemsPerPage: 5,
+        maxPages: 5,
+        modelValue: 2,
+      },
+    });
+
+    expect(
+      getByRole("button", { name: "Page 2", current: true })
+    ).toBeInTheDocument();
+    await fireEvent.click(getByRole("button", { name: "Previous page" }));
+    expect(
+      getByRole("button", { name: "Page 1", current: true })
+    ).toBeInTheDocument();
+  });
+
+  test("previous should be disabled with first page active", async () => {
+    const { getByRole } = render(Pagination, {
+      props: {
+        itemsCount: 40,
+        itemsPerPage: 5,
+        maxPages: 5,
+        modelValue: 1,
+      },
+    });
+
+    expect(
+      getByRole("button", { name: "Previous page" })
+    ).toHaveAttribute('aria-disabled', 'true');
+    expect(
+      getByRole("button", { name: "Previous page" })
+    ).toHaveClass('pointer-events-none');
+  });
+
+  test("next should be disabled with last page active", async () => {
+    const { getByRole } = render(Pagination, {
+      props: {
+        itemsCount: 40,
+        itemsPerPage: 5,
+        maxPages: 5,
+        modelValue: 8,
+      },
+    });
+
+    expect(
+      getByRole("button", { name: "Next page" })
+    ).toHaveAttribute('aria-disabled', 'true');
+    expect(
+      getByRole("button", { name: "Next page" })
+    ).toHaveClass('pointer-events-none');
+  });
+});
+
+test("click page should change to that page", async () => {
+  const { getByRole } = render(Pagination, {
+    props: {
+      itemsCount: 40,
+      itemsPerPage: 5,
+      maxPages: 5,
+    },
+  });
+
+  expect(
+    getByRole("button", { name: "Page 1", current: true })
+  ).toBeInTheDocument();
+  await fireEvent.click(getByRole("button", { name: "Page 3" }));
+  expect(
+    getByRole("button", { name: "Page 3", current: true })
+  ).toBeInTheDocument();
 });
