6ae38c05421ffd87e0a35461339e5cdfbf036d03
maciejg-git <ender@localhost>
2023-04-23 01:29:25 +0200

    update tests

--- src/test/Tabs.test.js
+++ src/test/Tabs.test.js
@@ -1,5 +1,5 @@
 import { render, fireEvent, waitFor } from "@testing-library/vue";
-import "@testing-library/jest-dom"
+import "@testing-library/jest-dom";
 import Tabs from "../components/vTabs.vue";
 import Tab from "../components/vTab.vue";
 
@@ -7,11 +7,17 @@ let tabs = [
   "<v-tab name='tab'>tab content</v-tab>",
   "<v-tab name='tab2'>tab2 content</v-tab>",
   "<v-tab name='tab3'>tab3 content</v-tab>",
+  `<v-tab>
+    <template #name>
+        tab custom
+    </template>
+    tab custom content
+  </v-tab>`,
 ];
 
 let stubs = {
   "v-tab": Tab,
-}
+};
 
 test("renders component", async () => {
   const { getByRole, getAllByRole } = render(Tabs, {
@@ -24,10 +30,10 @@ test("renders component", async () => {
     },
   });
 
-  expect(getByRole("tablist")).toBeInTheDocument()
+  expect(getByRole("tablist")).toBeInTheDocument();
   await waitFor(() => {
-    expect(getAllByRole("tab").length).toBe(3)
-  })
+    expect(getAllByRole("tab").length).toBe(4);
+  });
 });
 
 test("selects first tab by default", async () => {
@@ -42,8 +48,8 @@ test("selects first tab by default", async () => {
   });
 
   await waitFor(() => {
-    expect(getByRole("tabpanel")).toBeVisible()
-  })
+    expect(getByRole("tabpanel")).toBeVisible();
+  });
 });
 
 test("change to tab on click", async () => {
@@ -57,9 +63,45 @@ test("change to tab on click", async () => {
     },
   });
 
-  let tab = await findAllByRole("tab")
-  await fireEvent.click(tab[1])
+  let tab = await findAllByRole("tab");
+  await fireEvent.click(tab[1]);
   await waitFor(() => {
-    expect(queryByRole("tabpanel")).toHaveTextContent("tab2 content")
-  })
+    expect(queryByRole("tabpanel")).toHaveTextContent("tab2 content");
+  });
+});
+
+test("renders tab name in name slot", async () => {
+  const { getByRole, getAllByRole, findAllByRole, queryByRole } = render(Tabs, {
+    props: {},
+    slots: {
+      default: tabs,
+    },
+    global: {
+      stubs,
+    },
+  });
+
+  let tab = await findAllByRole("tab");
+  await fireEvent.click(tab[3]);
+  await waitFor(() => {
+    expect(queryByRole("tabpanel")).toHaveTextContent("tab custom content");
+  });
+  expect(tab[3]).toHaveTextContent("tab custom");
+});
+
+test("emits input:changed-tab on tab change", async () => {
+  const { getByRole, getAllByRole, findAllByRole, emitted } = render(Tabs, {
+    props: {},
+    slots: {
+      default: tabs,
+    },
+    global: {
+      stubs,
+    },
+  });
+
+  expect(emitted('input:changed-tab')[0][0]).toBe(0)
+  let tab = await findAllByRole("tab");
+  await fireEvent.click(tab[2]);
+  expect(emitted('input:changed-tab')[1][0]).toBe(2)
 });
