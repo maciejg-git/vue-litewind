79fd0f541ecc2cb39ba4db92ee9dffc13c57c9b3
maciejg-git <ender@localhost>
2023-05-19 00:37:21 +0200

    add auto column break componente

--- src/components/index.js
+++ src/components/index.js
@@ -38,6 +38,7 @@ export { default as vFluidContainer } from "./vFluidContainer.vue"
 export { default as vContainer } from "./vContainer.vue"
 export { default as vRow } from "./vRow.vue"
 export { default as vAutoCol } from "./vAutoCol.vue"
+export { default as vAutoColBreak } from "./vAutoColBreak.vue"
 export { default as vCol } from "./vCol.vue"
 
 export { default as vForm } from "./vForm.vue"
