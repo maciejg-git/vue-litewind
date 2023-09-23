4a48cedc26670413a0c83f18cb5d2d5e18876d86
maciejg-git <ender@localhost>
2023-09-24 00:52:04 +0200

    update vite config

--- vite-docs.config.js
+++ vite-docs.config.js
@@ -1,8 +1,34 @@
 import { defineConfig } from "vite";
 import vue from "@vitejs/plugin-vue";
+import Markdown from "unplugin-vue-markdown/vite";
+import MarkdownItAnchor from 'markdown-it-anchor'
+import MarkdownItAttrs from 'markdown-it-attrs'
+import hljs from "highlight.js"
 
 export default defineConfig({
-  plugins: [vue()],
+  plugins: [
+    vue({
+      include: [/\.vue$/, /\.md$/],
+    }),
+    Markdown({
+      markdownItOptions: {
+        langPrefix: 'hljs language-',
+        highlight: function (str, lang) {
+          if (lang && hljs.getLanguage(lang)) {
+            try {
+              return hljs.highlight(str, { language: lang }).value;
+            } catch (__) {}
+          }
+
+          return ''; // use external default escaping
+        },
+      },
+      markdownItSetup(md) {
+        md.use(MarkdownItAnchor)
+        md.use(MarkdownItAttrs)
+      },
+    }),
+  ],
   build: {
     outDir: "dist-docs",
     rollupOptions: {},
