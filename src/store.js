31a6b00a493d87304f67b35ce3c2e02d2a0ae92d
maciejg-git <ender@localhost>
2023-09-24 00:51:54 +0200

    update store

--- src/store.js
+++ src/store.js
@@ -1,9 +1,10 @@
 import { defineStore } from 'pinia'
+import { ref } from "vue"
 
-export const useStore = defineStore("store", {
-  state: () => {
-    return {
-      filter: "",
-    }
+export const useStore = defineStore("store", () => {
+  let filter = ref("")
+
+  return {
+    filter,
   }
 })
