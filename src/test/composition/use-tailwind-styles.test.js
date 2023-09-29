da48ad0cfd7826f80f9ae5fecf861aaa70ff3c5f
maciejg-git <ender@localhost>
2023-09-30 00:52:48 +0200

    update tests

---
+++ src/test/composition/use-tailwind-styles.test.js
@@ -0,0 +1,218 @@
+import useTailwindStyles from "../../components/composition/use-tailwind-styles";
+import { reactive } from "vue";
+
+let global = {
+  provide: {
+    mods: {
+      test: {
+        test: {
+          element: {
+            classes: `
+              static
+              static2
+            `,
+            size: {
+              medium: `
+              medium
+              medium2
+              `,
+              small: `
+              small
+              small2
+              `,
+            },
+            variant: {
+              classes: `
+              variant
+              variant2
+              `,
+              primary: `
+              primary
+              primary2
+              `,
+              secondary: `
+              secondary
+              secondary2
+              `,
+            },
+            effect: {
+              ring: `
+                ring
+                ring2
+              `,
+            },
+            special: {
+              optional: true,
+              raised: `
+              raised
+              raised2
+              `,
+            },
+            preset: {
+              plain: `
+              plain
+              plain2
+              `,
+            },
+          },
+          element2: {
+            classes: `
+              static
+              static2
+            `,
+            size: {
+              medium: `
+              medium
+              medium2
+              `,
+              small: `
+              small
+              small2
+              `,
+            },
+            variant: {
+              classes: `
+              variant
+              variant2
+              `,
+              primary: `
+              primary
+              primary2
+              `,
+              secondary: `
+              secondary
+              secondary2
+              `,
+            },
+            effect: {
+              ring: `
+                ring
+                ring2
+              `,
+            },
+            special: {
+              optional: true,
+              raised: `
+              raised
+              raised2
+              `,
+            },
+            preset: {
+              plain: `
+              plain
+              plain2
+              `,
+            },
+          },
+        },
+      },
+    },
+  },
+};
+
+let elements = {
+  element: {
+    fixed: "element-fixed element-fixed2",
+  },
+  element2: {
+    fixed: "element2-fixed element2-fixed2",
+  },
+};
+
+const props = reactive({
+  modElement: "",
+  modElement2: "",
+  base: "test",
+  variant: "",
+});
+
+afterEach(() => {
+  props.modElement = "";
+  props.modElement2 = "";
+  props.base = "test";
+  props.variant = "";
+});
+
+describe("adds classes", () => {
+  test("fixed", () => {
+    let { classes, variants } = useTailwindStyles(
+      props,
+      global.provide.mods.test,
+      elements
+    );
+
+    expect(classes.element.value).toMatch(/element-fixed element-fixed2/);
+    expect(classes.element2.value).toMatch(/element2-fixed element2-fixed2/);
+  });
+
+  test("element classes (classes property)", () => {
+    let { classes, variants } = useTailwindStyles(
+      props,
+      global.provide.mods.test,
+      elements
+    );
+
+    expect(classes.element.value).toMatch(/static static2/);
+    expect(classes.element2.value).toMatch(/static static2/);
+  });
+
+  test("default variants classes", () => {
+    let { classes, variants } = useTailwindStyles(
+      props,
+      global.provide.mods.test,
+      elements
+    );
+
+    expect(classes.element.value).toMatch(
+      /.*medium medium2.*primary primary2.*ring ring2/
+    );
+    expect(classes.element2.value).toMatch(
+      /.*medium medium2.*primary primary2.*ring ring2/
+    );
+  });
+});
+
+test("does not add optional variants classes", () => {
+  let { classes, variants } = useTailwindStyles(
+    props,
+    global.provide.mods.test,
+    elements
+  );
+
+  expect(classes.element.value).not.toMatch(/raised raised2/);
+  expect(classes.element2.value).not.toMatch(/raised raised2/);
+});
+
+test("adds mod-element prop classes", () => {
+  props.modElement = "variant:secondary size:small";
+  props.modElement2 = "variant:secondary size:small";
+
+  let { classes, variants } = useTailwindStyles(
+    props,
+    global.provide.mods.test,
+    elements
+  );
+
+  expect(classes.element.value).toMatch(/secondary secondary2/);
+  expect(classes.element2.value).toMatch(/secondary secondary2/);
+  expect(classes.element.value).toMatch(/small small2/);
+  expect(classes.element2.value).toMatch(/small small2/);
+  expect(classes.element.value).not.toMatch(/primary primary2/);
+  expect(classes.element.value).not.toMatch(/primary primary2/);
+  expect(classes.element2.value).not.toMatch(/medium medium2/);
+  expect(classes.element2.value).not.toMatch(/medium medium2/);
+});
+
+test("adds only preset classes", () => {
+  props.modElement = "preset:plain";
+  props.modElement2 = "preset:plain";
+
+  let { classes, variants } = useTailwindStyles(
+    props,
+    global.provide.mods.test,
+    elements
+  );
+
+  expect(classes.element.value).toMatch("plain plain2");
+  expect(classes.element2.value).toMatch("plain plain2");
+});
