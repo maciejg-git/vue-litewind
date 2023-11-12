1659d6708ee95e24765e7812dafbcd84fc61ba3e
maciejg-git <ender@localhost>
2023-11-12 03:38:39 +0100

    update docs

---
+++ src/documentation/examples/ExampleProgressIndeterminateStyle.vue
@@ -0,0 +1,8 @@
+<template>
+  <v-progress
+    mod-progress="size:tiny width:long easing:inOut"
+    mod-progress-bar="variant:gradient"
+    indeterminate
+    class="my-8"
+  ></v-progress>
+</template>
