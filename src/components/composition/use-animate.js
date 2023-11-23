721e1d34f2fadd4a4cf4e8f0331cd8a6d7e6e26d
maciejg-git <ender@localhost>
2023-11-24 00:14:22 +0100

    update useAnimate

--- src/components/composition/use-animate.js
+++ src/components/composition/use-animate.js
@@ -1,21 +1,26 @@
 let remap = (v, range) => (v * (range[1] - range[0])) / 1 + range[0];
+let clamp = (v) => (v < 0 ? 0 : v > 1 ? 1 : v);
 
-let animationDefaults = {
-  _cycles: 0,
-  _frame: 0,
-  _durationOffset: 0,
-}
+let defaultAnimationState = {
+  timeFraction: 0,
+  totalFraction: 0,
+  frame: 0,
+  reverse: false,
+  cycles: 0,
+  frameOffset: 0,
+};
 
 export default function useAnimate() {
   let state = "stop";
-  let startTime = 0, pausedTime = 0;
+  let startTime = 0;
+  let pausedTime = 0;
   let animations = [];
 
   let play = () => {
     if (state === "play") return;
     startTime = performance.now() - pausedTime;
     state = "play";
-    animations.forEach((animation) => animate(animation))
+    animations.forEach((animation) => animate(animation));
   };
 
   let stop = () => {
@@ -23,8 +28,8 @@ export default function useAnimate() {
     pausedTime = 0;
     animations.forEach((animation, i) => {
       cancelAnimationFrame(animation.reqId)
-      animations[i] = {...animation, ...animationDefaults}
-    })
+      animations[i].state = { ...defaultAnimationState }
+    });
   };
 
   let pause = () => {
@@ -33,18 +38,6 @@ export default function useAnimate() {
     pausedTime = performance.now() - startTime;
     animations.forEach((animation) => cancelAnimationFrame(animation.reqId))
   };
-  // let pause = () => {
-  //   // if (state === states.PAUSE || state === states.STOP) return;
-  //   // state = states.PAUSE;
-  //   // pausedTime = performance.now() - startTime;
-  //   if (state === states.PAUSE) {
-  //     state = states.START
-  //     requestId = requestAnimationFrame(animate)
-  //   } else {
-  //     state = states.PAUSE
-  //     cancelAnimationFrame(requestId);
-  //   }
-  // };
 
   let set = (animation) => {
     animations = (Array.isArray(animation) ? [...animation] : [animation])
@@ -52,59 +45,69 @@ export default function useAnimate() {
         return { ...i };
       })
       .map((i) => {
-        if (!i.duration[1]) i.duration = [0, i.duration];
         i._isAlternate =
           i.direction === "alternate" || i.direction === "alternate-reverse";
         i._isReverse =
           i.direction === "reverse" || i.direction === "alternate-reverse";
-        i._reverse = i._isReverse;
-        i.repeat = i.repeat === true ? 9999999 : +i.repeat
-        if (Array.isArray(i?.frames)) i._frames = [...i.frames]
-        else i._frames = Array.from({length: i.frames || 1}, () => ({ duration: i.duration[1]/(i.frames || 1) }))
+        i.repeat = i.repeat === true ? 9999999 : +i.repeat;
+        if (Array.isArray(i?.frames)) i._frames = [...i.frames];
+        else
+          i._frames = Array.from({ length: i.frames || 1 }, () => ({
+            duration: i.duration / (i.frames || 1),
+          }));
         i._frames = i._frames.map((f) => {
-          if (!f.duration) f.duration = 0
-          if (!f.timing) f.timing = i.timing
-          return f
-        })
-        return { ...i, ...animationDefaults };
+          if (!f.duration) f.duration = 0;
+          if (!f.timing) f.timing = i.timing;
+          return f;
+        });
+        i.state = { ...defaultAnimationState }
+        i.state.reverse = i._isReverse
+        return i;
       });
   };
 
   let destroy = () => stop();
 
   let animate = (animation) => {
-  let step = (time) => {
-      let frame = animation._frames[animation._frame]
-      let timeFraction = (time - startTime - animation._durationOffset - animation.duration[0]) / frame.duration
+    let { state } = animation;
 
-      if (animation._reverse) timeFraction = 1 - timeFraction;
+    let step = (time) => {
+      let frame = animation._frames[state.frame];
+      let elapsed = time - startTime
+      state.totalFraction = elapsed / animation.duration;
+      state.timeFraction =
+        (elapsed - state.frameOffset) / frame.duration;
 
-      timeFraction = timeFraction < 0 ? 0 : timeFraction > 1 ? 1 : timeFraction;
+      state.timeFraction = clamp(state.timeFraction);
+      state.totalFraction = clamp(state.totalFraction) - state.cycles;
 
-      let progress = frame.timing(timeFraction);
+      if (state.reverse) state.timeFraction = 1 - state.timeFraction;
+
+      let progress = frame.timing(state.timeFraction);
 
       if (animation.remap) progress = remap(progress, animation.remap);
 
-      animation.draw(progress, timeFraction, animation._frame);
+      animation.draw(progress, state);
 
-      if ((!animation._reverse && timeFraction === 1) ||
-        (animation._reverse && timeFraction === 0)) {
-        animation._durationOffset += frame.duration
-        if (animation.newFrame) animation.newFrame(animation._frame)
-        if (++animation._frame >= animation._frames.length) {
-          animation._frame = 0
+      if (
+        (!state.reverse && state.timeFraction === 1) ||
+        (state.reverse && state.timeFraction === 0)
+      ) {
+        state.frameOffset += frame.duration;
+        if (++state.frame >= animation._frames.length) {
+          state.frame = 0;
           if (animation.repeat) {
-            animation._cycles++
+            state.cycles++;
             if (animation._isAlternate) {
-              animation._reverse = (animation._cycles + animation._isReverse) % 2
+              state.reverse = (state.cycles + animation._isReverse) % 2;
             }
-            animation.reqId = requestAnimationFrame(step)
+            animation.reqId = requestAnimationFrame(step);
           }
-        } else animation.reqId = requestAnimationFrame(step)
-      } else animation.reqId = requestAnimationFrame(step)
+        } else animation.reqId = requestAnimationFrame(step);
+      } else animation.reqId = requestAnimationFrame(step);
+    };
+    animation.reqId = requestAnimationFrame(step);
   };
-  animation.reqId = requestAnimationFrame(step) 
-  }
 
   return {
     play,
