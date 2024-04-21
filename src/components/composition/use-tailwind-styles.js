3224a437d1bc716aff6fcf0a2506689d2010bdcc
maciejg-git <ender@localhost>
2024-04-22 00:01:24 +0200

    refactor useTailwindStyles

--- src/components/composition/use-tailwind-styles.js
+++ src/components/composition/use-tailwind-styles.js
@@ -3,15 +3,70 @@ import { ref, computed } from "vue";
 let isFunction = (v) => typeof v === "function";
 
 let setStyleVar = (variables) => {
-  let root = document.querySelector(":root")
+  let root = document.querySelector(":root");
 
   Object.entries(variables).forEach((i) => {
-    root.style.setProperty(i[0], i[1])
-  })
-}
+    root.style.setProperty(i[0], i[1]);
+  });
+};
 
 let getClasses = (classes) => {
   return isFunction(classes) ? classes() : classes;
+};
+
+let getExternalVariants = (element, options, styles, base) => {
+  let externalVariants = options.externalVariants.reduce((acc, variant) => {
+    acc = { ...acc, ...styles[base][element][variant] };
+    return acc;
+  }, {});
+
+  for (let variant in externalVariants) {
+    if (variant === "optional") continue;
+    externalVariants[variant] = externalVariants[variant].replace(
+      /\s\s+/g,
+      " "
+    );
+  }
+
+  return externalVariants;
+};
+
+let getActiveMods = (elementStyles, mods, state) => {
+  let activeMods = {};
+
+  if (mods.preset) {
+    activeMods.preset = mods.preset[0];
+  }
+
+  for (let type in elementStyles) {
+    if (type[0] === "_" || type === "preset" || type === "classes") continue;
+
+    if (mods[type]) {
+      mods[type].forEach((i) => {
+        activeMods[type] = { ...activeMods[type], [i]: true };
+      });
+      continue;
+    }
+
+    if (elementStyles[type][state.value]) {
+      activeMods[type] = {
+        ...activeMods[type],
+        [elementStyles[type][state.value]]: true,
+      };
+    }
+
+    if (elementStyles[type].optional) {
+      continue;
+    }
+
+    for (let item in elementStyles[type]) {
+      if (item === "classes") continue;
+      activeMods[type] = { ...activeMods[type], [item]: true };
+      break;
+    }
+  }
+
+  return activeMods;
 };
 
 let parseElementModProp = (props, el) => {
@@ -27,7 +82,7 @@ let parseElementModProp = (props, el) => {
       return acc;
     }
 
-    if (!acc[item[2]]) acc[item[2]] = []
+    if (!acc[item[2]]) acc[item[2]] = [];
     acc[item[2]].push(item[3]);
 
     return acc;
@@ -42,70 +97,32 @@ export default function useTailwindStyles(props, styles, elements) {
 
   let state = ref("");
 
-  if (styles.cssVariables) setStyleVar(styles.cssVariables)
+  if (styles.cssVariables) setStyleVar(styles.cssVariables);
 
   groupClass = styles?.[props.base]?._options?.containerGroupClass ?? "";
 
   for (let [element, options] of Object.entries(elements)) {
-
-    variants[element] = {};
+    if (options?.externalVariants) {
+      variants[element] = getExternalVariants(
+        element,
+        options,
+        styles,
+        props.base
+      );
+    }
 
     classes[element] = computed(() => {
-      let activeMods = {}
-
       if (!styles[props.base]) return "";
 
       let classes = "";
 
-      let mods = parseElementModProp(props, element)
+      let mods = parseElementModProp(props, element);
 
       let elementStyles = styles[props.base][element];
 
       // get active mods
 
-      if (mods.preset) {
-        activeMods.preset = mods.preset[0]
-      }
-
-      for (let type in elementStyles) {
-        if (type[0] === "_" || type === "preset" || type === "classes") continue
-
-        if (mods[type]) {
-          mods[type].forEach((i) => {
-            activeMods[type] = {...activeMods[type], [i]: true}
-          })
-          continue
-        }
-
-        if (elementStyles[type][state.value]) {
-          activeMods[type] = {...activeMods[type], [elementStyles[type][state.value]]: true}
-        }
-
-        if (elementStyles[type].optional) {
-          continue
-        }
-
-        for (let item in elementStyles[type]) {
-          if (item === "classes") continue
-          activeMods[type] = {...activeMods[type], [item]: true}
-          break
-        }
-      }
-
-      for (let type in elementStyles) {
-        if (type[0] === "_") continue;
-
-        if (options?.externalVariants?.includes(type)) {
-          for (let variant in elementStyles[type]) {
-            if (variant === "optional") continue
-            variants[element][variant] = elementStyles[type][variant].replace(
-              /\s\s+/g,
-              " "
-            );
-          }
-          continue;
-        }
-      }
+      let activeMods = getActiveMods(elementStyles, mods, state);
 
       // return classes
 
@@ -125,6 +142,10 @@ export default function useTailwindStyles(props, styles, elements) {
         dataStyle[element] = elementStyles.data;
       }
 
+      if (elementStyles.state) {
+        classes += state.value ? elementStyles.state[state.value] : null;
+      }
+
       for (let type in elementStyles) {
         if (type[0] === "_") continue;
         let sharedClasses = null;
@@ -136,25 +157,30 @@ export default function useTailwindStyles(props, styles, elements) {
           continue;
         }
 
-        if (type !== "preset" && type !== "data" && type !== "classes") {
-          stateClasses = state.value ? elementStyles[type][state.value] : null;
+        if (
+          type === "preset" ||
+          type === "data" ||
+          type === "classes" ||
+          type === "state"
+        ) {
+          continue;
+        }
 
-          if (type !== "state") {
-            sharedClasses = elementStyles[type]?.classes || "";
+        stateClasses = state.value ? elementStyles[type][state.value] : null;
 
-            if (mods[type]) {
-              mods[type].forEach((i) => {
-                modClasses += elementStyles[type][i]
-              })
-            }
+        sharedClasses = elementStyles[type]?.classes || "";
 
-            if (!elementStyles[type].optional) {
-              for (let item in elementStyles[type]) {
-                if (item === "classes") continue;
-                defaultClasses = elementStyles[type][item];
-                break;
-              }
-            }
+        if (mods[type]) {
+          mods[type].forEach((i) => {
+            modClasses += elementStyles[type][i];
+          });
+        }
+
+        if (!elementStyles[type].optional) {
+          for (let item in elementStyles[type]) {
+            if (item === "classes") continue;
+            defaultClasses = elementStyles[type][item];
+            break;
           }
         }
 
@@ -166,9 +192,9 @@ export default function useTailwindStyles(props, styles, elements) {
 
       return `
         ${classes}
-        ${(options?.fixed || "")}
-        ${(options?.computed?.value || "")}
-      `.replace(/\s\s+/g, " ")
+        ${options?.fixed || ""}
+        ${options?.computed?.value || ""}
+      `.replace(/\s\s+/g, " ");
     });
   }
 
